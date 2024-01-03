9f0c40e46a003ee5f8979578b2466f6aaefee23f
gu.zheng <guzheng16399@163.com>
2024-01-03 23:21:51 +0800

    2024_1_3 后端项目初始化

---
+++ service-web/src/main/java/org/kooguy/work/BlogWebRestApplication.java
@@ -0,0 +1,44 @@
+package org.kooguy.work;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
+@SpringBootApplication(scanBasePackages = "org.kooguy.work")
+public class BlogWebRestApplication {
+    public static void main(String[] args) throws UnknownHostException {
+        System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true");
+        ConfigurableApplicationContext application = SpringApplication.run(BlogWebRestApplication.class, args);
+        Environment env = application.getEnvironment();
+
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "应用 '{}' 运行成功! 访问连接:\n\t" +
+                        "Swagger文档: \t\thttp://{}:{}{}{}/swagger-ui/index.html\n\t" +
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port", "8080"),
+                env.getProperty("server.servlet.context-path", ""),
+                env.getProperty("spring.mvc.servlet.path", "")
+        );
+    }
+
+    @Bean
+    public ServletWebServerFactory webServerFactory(){
+        TomcatServletWebServerFactory fa = new TomcatServletWebServerFactory();
+        fa.addConnectorCustomizers(connector -> {
+            connector.setProperty("relaxedQueryChars","%_(),/:;<>=?$@[\\]{}");
+            connector.setProperty("rejectIllegalHeader","false");
+        });
+        return fa;
+    }
+}
